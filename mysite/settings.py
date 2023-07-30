a70a267b2f0b984a38477b761191a0bb443a63b3
otienobrax <otienobraxtonn@gmail.com>
2023-07-30 12:07:43 +0300

    django 101 commit 2

--- mysite/settings.py
+++ mysite/settings.py
@@ -23,9 +23,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-f@b9nf_+aa97+&xwdg@eac!r*uhehdk3i)_0k0-6ld@+_9^wg$'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
